a6692591f2d199685082b4471ed86ae6dff6094d
mkleung-pcms <116505065+mkleung-pcms@users.noreply.github.com>
2022-11-06 20:50:18 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.magneticForce(Dimension.X))
     status = 3 - status
 })
+input.onButtonPressed(Button.AB, function () {
+    input.calibrateCompass()
+})
 input.onButtonPressed(Button.B, function () {
     status = 1 - status
 })
@@ -12,12 +15,12 @@ status = 0
 basic.forever(function () {
     if (status == 0) {
         led.setBrightness(128)
-        basic.clearScreen()
-        for (let index = 0; index <= Math.round(input.magneticForce(Dimension.X) * 0.09375 + 1); index++) {
-            led.plot(Math.trunc((index - 1) / 5), (index - 1) % 5)
-        }
+        led.plotBarGraph(
+        Math.abs(input.magneticForce(Dimension.X)),
+        500
+        )
     } else if (status == 1) {
-        led.setBrightness(5 + input.magneticForce(Dimension.X) * 0.9765625)
+        led.setBrightness(5 + Math.abs(input.magneticForce(Dimension.X)) * 0.5)
         basic.showLeds(`
             # # # # #
             # # # # #
